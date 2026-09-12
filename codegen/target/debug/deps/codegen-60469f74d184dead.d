2176cf3e2e63146e39fb76798804f502b0e83de7
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-09-12 02:59:52 -0700

    Whoops, forgot to do gitignore for target directory.

--- codegen/target/debug/deps/codegen-60469f74d184dead.d
+++
@@ -1,6 +0,0 @@
-/home/null/docs/write-ups/voxel-orientations/codegen/target/debug/deps/codegen-60469f74d184dead.d: src/main.rs src/direction.rs
-
-/home/null/docs/write-ups/voxel-orientations/codegen/target/debug/deps/libcodegen-60469f74d184dead.rmeta: src/main.rs src/direction.rs
-
-src/main.rs:
-src/direction.rs:
